48c9e98c0b980634e9eac9fca5b303b04e61472a
PeerapongMala <151112918+PeerapongMala@users.noreply.github.com>
2024-07-24 11:07:07 +0700

    a

--- src/main/java/th/mfu/HelloController.java
+++ src/main/java/th/mfu/HelloController.java
@@ -1,5 +1,8 @@
 package th.mfu;
 
+import javax.annotation.Generated;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/secure")
     String hello() {
         return "Hello World!";
     }
 
-   
 }
